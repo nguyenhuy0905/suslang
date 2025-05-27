9d03c35624fba6ee0d5f7695213f8c1fb4f49397
Huy Nguyen <huy55465@gmail.com>
2025-05-26 20:34:32 -0400

    feat(parse): new parse error type, start stmt
    Commit-by: Huy Nguyen

--- parse/src/lib.rs
+++ parse/src/lib.rs
@@ -1,8 +1,10 @@
-mod expr;
+pub mod expr;
+pub mod stmt;
 pub use expr::*;
 use std::any::Any;
 use std::collections::VecDeque;
 use std::fmt::Debug;
+pub use stmt::*;
 use tokenize::Token;
 
 /// Empty for now, but in case I need something down the line.
@@ -64,6 +66,7 @@ pub enum ParseError {
     UnexpectedToken(Token),
     ExpectedToken { line: usize, pos: usize },
     UnclosedParen { line: usize, pos: usize },
+    UnendedStmt { line: usize, pos: usize },
 }
 
 // when are you gonna finish your language
