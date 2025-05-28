e23a39766def9530f6ff9a6a59276986cd63798e
Huy Nguyen <huy55465@gmail.com>
2025-05-28 18:34:20 -0400

    feat(parse): kinda done with NameResolve.
    Commit-by: Huy Nguyen

--- parse/src/stmt.rs
+++ parse/src/stmt.rs
@@ -13,6 +13,9 @@ mod test;
 /// Contains info about a scope: what symbols are declared, what other scopes
 /// are pulled in, what the parent scope is.
 ///
+/// A child module inherits everything from its parent (in this goofy language
+/// that's called "overlord" for some reason).
+///
 /// This struct is used for both block scope and module scope. If you think
 /// about it, a module is pretty much in the form of, "mod modname {}".
 /// If that doesn't look like a block scope to me, I dunno what does.
@@ -65,9 +68,95 @@ impl Clone for TypeInfoKind {
 /// Scope and type resolution.
 /// The first step can be `ResolveStep::Global`, but all the other can only be
 /// `Parent` or `Child`.
+///
+/// Unless the first step is `Global`, the resolution starts from the current
+/// module.
+///
+/// # Rule
+/// \<type-ref\> ::= "::"? ID ("::" ID)*
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct NameResolve {
     pub resolve: Vec<ResolveStep>,
+}
+
+#[macro_export]
+macro_rules! new_name_resolve {
+    ($($elem:expr),+ $(,)?) => {
+        NameResolve {
+            resolve: vec![ResolveStep::$($elem)+]
+        }
+    };
+}
+
+impl NameResolve {}
+
+impl Type for NameResolve {}
+
+impl TypeParse for NameResolve {
+    /// Need to make sure `tokens` is not empty before calling this function.
+    fn parse(tokens: &mut VecDeque<Token>) -> Result<TypeInfoKind, ParseError> {
+        assert!(!tokens.is_empty());
+        // first (one or two) token(s) must be some type or module, or "::"
+        // followed by a type or module.
+        // If the next token is "::" then its next token must be another type
+        // or module.
+        // If the next token is something else then we could say name
+        // resolution ends.
+
+        let mut resolve = Vec::new();
+        // get the first resolve step
+        let (tok_typ, line, pos) = tokens.pop_front().map(Token::bind).unwrap();
+        match tok_typ {
+            TokenType::ColonColon => {
+                resolve.push(ResolveStep::Global);
+                // So, bringing global module to scope is kinda stupid.
+                // Since it's in scope already anyways.
+                let Some((tok_typ, line, pos)) =
+                    tokens.pop_front().map(Token::bind)
+                else {
+                    return Err(ParseError::ExpectedToken { line, pos });
+                };
+                if let TokenType::Identifier(s) = tok_typ {
+                    resolve.push(ResolveStep::Child(s));
+                } else {
+                    return Err(ParseError::UnexpectedToken(Token::new(
+                        tok_typ, line, pos,
+                    )));
+                }
+            }
+            TokenType::Identifier(s) => resolve.push(ResolveStep::Child(s)),
+            TokenType::Overlord => resolve.push(ResolveStep::Parent),
+            _ => {
+                return Err(ParseError::UnexpectedToken(Token::new(
+                    tok_typ, line, pos,
+                )));
+            }
+        }
+        // get the following resolve steps, if any
+        while matches!(
+            tokens.front().map(Token::token_type),
+            Some(&TokenType::ColonColon)
+        ) {
+            let (_, line, pos) = tokens.pop_front().map(Token::bind).unwrap();
+            // following a ColonColon must be an Identifier
+            if tokens.is_empty() {
+                return Err(ParseError::ExpectedToken { line, pos });
+            }
+            let (tok_typ, line, pos) =
+                tokens.pop_front().map(Token::bind).unwrap();
+            match tok_typ {
+                TokenType::Identifier(s) => resolve.push(ResolveStep::Child(s)),
+                TokenType::Overlord => resolve.push(ResolveStep::Parent),
+                _ => {
+                    return Err(ParseError::UnexpectedToken(Token::new(
+                        tok_typ, line, pos,
+                    )))
+                }
+            }
+        }
+
+        Ok(TypeInfoKind::Reference(Self { resolve }))
+    }
 }
 
 /// A single step in name resolution.
@@ -199,7 +288,7 @@ impl AsRef<dyn StmtImpl> for StmtAstBoxWrap {
 /// Variable declaration statement.
 ///
 /// # Rule
-/// \<var-decl\> ::= "let" "mut"? ID (":" TYPE_ID) "=" \<expr\>
+/// \<var-decl\> ::= "let" "mut"? ID (":" \<type-ref\>) "=" \<expr\>
 ///
 /// # Note
 /// - When parsing, TYPE_ID is equivalent to [`TypeInfoKind::Reference`].
