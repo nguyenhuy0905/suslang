fecf43b7705d8ccc00dd47c49734cef94a0f6648
Huy Nguyen <huy55465@gmail.com>
2025-06-29 18:29:34 -0400

    BREAKING CHANGE: nuke codebase
    Signed-off-by: Huy Nguyen <huy55465@gmail.com>

--- TODO.md
+++ TODO.md
@@ -1,56 +1,82 @@
 ## Todo
 
-- Last updated June 11th 2025.
-  - [x] Add error test cases for LetStmt.
-  - [x] Add error test cases for BlockExpr.
-  - [x] Implement a proc-call expr with equal precedence to the current highest
-    (that is, PrimaryExpr).
-  - [x] Add more error test cases for ProcCallExpr (specifically, trailing-comma
-    case).
-  - [x] Implement if-elif-else expr.
-    - [x] Define structs and grammar rules.
-    - [x] Define `new_from` for all types of branches,
-      - [x] `IfBranch`
-      - [x] `ElifBranch`
-      - [x] `ElseBranch`
-    - [x] Test it out!
-      - [x] Make nice macros like I have always done.
-        - This time, it's not as nice, but that's because this construct is
-          quite complex
-      - [x] Then write tests.
-        - [x] Write simplest-case checks.
-        - [x] Write error cases
-  - [x] Implement while expr.
-    - [x] Add a new keyword, "while".
-    - [x] Implement the thing.
-    - [x] Test it out:
-      - [x] Non-error case.
-      - [x] Error case.
-  - [x] Implement a block-escape statement.
-    - [x] Add a new keyword, "block_return".
-    - [x] Add BlockReturnStmt. Technically similar to ReturnStmt, but the keyword
-          is "block_return"
-    - [x] Test it out
-      - [x] Just copy-paste ReturnStmt's tests but change the keyword, dawg.
-  - [x] Quick fix: assignment statement
-    - [x] This has some overlap with `ExprValStmt`, so, make a new rule that
-          simply delegates.
-    - [x] Then implement `AssignStmt`.
-    - [x] Then write some tests.
-      - [x] Add some normie cases.
-      - [x] Add some error cases. (TODO: i only just added one error case)
-  - [ ] Start rolling the dummy semantics analysis system.
-    - [ ] Assign blocks to scopes.
-      - Given my current implementation, what I'm thinking of is:
-        - [ ] Create a new type `ScopedBlock`. Which is still an `ExprAst`, but
-              this time is NOT an `ExprParse`. Well, because one shouldn't be able
-              to parse it.
-        - [ ] And, convert all `BlockExpr` to `ScopedBlock`.
-          - [ ] If it's wrapped behind an `ExprBoxWrap`, tryna downcast it.
-            - Man, I shoulda used enums instead.
-        - Of course, this is a noble cause for "refactoring," but I'm not gonna
-          refactor again until I finish everything I set out to do.
-          - At least now I learn that this OOP-styled thingy kinda stinks.
-    - Note: no need for type check, even if I have type annotation. That stuff
-      happens run-time.
-  - What's next: finish all the items above first.
+- This is another codebase nuke.
+- The OOP-ish way before irritated me quite a lot. Now Imma enum.
+  - Why, because I *need* to know the underlying type.
+  - I can try to cope by adding all the `Into<SomeType>` stuff. But that's
+    a lot of maintenance: every time I add a new struct, I need to, for all
+    other structs, add another `Into`.
+  - Or, a visitor, whose best implementation is, well, an enum.
+- Anyways, enough rant, onto the actual to-do.
+
+- [ ] Enum `Expr`:
+  - [ ] `Primary`, which is another enum:
+    - [ ] `Identifier`
+    - [ ] `String`
+    - [ ] `Integer`
+    - [ ] `Float`
+    - [ ] `Boolean`
+    - [ ] `ProcCall`
+    - If meeting a `()`, simply delegate back to `Expr`'s parse function.
+  - [ ] `Unary`
+  - [ ] `Factor`
+  - [ ] `Term`
+  - [ ] `BitAnd`
+  - [ ] `BitXor`
+  - [ ] `BitOr`
+  - [ ] `Comparison`
+  - [ ] `LogicAnd`
+  - [ ] `LogicOr`
+
+- [ ] Enum `Stmt`:
+  - [ ] `Expr` statement
+    - [ ] `Return`, `BlockReturn`
+    - [ ] `Semicolon` (an `Expr` followed by a semicolon)
+  - [ ] `Let` statement
+    - Do I add a `Def`/`Const` statement for all immutable types (say, function
+      decl)?
+    - Or, `Let` with `Mut`?
+      - If not `Mut`, allow some more declaration types?
+        - Need a new enum for that probably.
+      - If `Mut`, just same-old expression.
+
+- [ ] Extend `Expr`:
+  - [ ] `Block`
+    - Should I start assigning block scope here?
+    - At this stage, there should be enough info already. I can look for any
+      `Let` statements (or `Def`/`Const` if I also add them).
+  - [ ] `Proc`
+    - If I do go the route of `Def` stmt, `Proc` should be in a different enum
+      type than `Expr`.
+    - In its place here can be, say, `Lambda`.
+  - [ ] `If`
+    - [ ] `IfBranch`
+    - [ ] `ElseBranch`
+    - [ ] `ElifBranch`
+  - [ ] `While`
+
+- Should `Assignment` be an `Expr` or `Stmt`?
+
+- Do I add type info?
+  - Do I add type info *during* tokenization instead?
+  - If yes, think about module or namespace system, and generics
+    - How to declare a module/namespace?
+      - Explicit declaration?
+      - Or based on file name?
+    - How to refer to a type?
+      - I will need to modify how `Identifier` is parsed.
+      - In the same module/namespace?
+      - In a different module/namespace?
+        - Import a module/namespace?
+        - Relative path to refer to children modules/namespaces?
+        - Aliasing? With `Let` or `Def`/`Const`?
+    - Sizes of types?
+    - Generics? Conversion between types? Or simply type annotation that gets
+      checked in runtime?
+      - Name mangling?
+  - If I wanna make this into a query language (with flares), Imma just rely on
+    runtime type checking.
+
+- [ ] Semantics analysis
+  - [ ] Assign scope to blocks
+    - Or, do I do this way up during parsing a `Block`?
--- parse/src/lib.rs
+++ parse/src/lib.rs
@@ -1,30 +1,229 @@
-pub mod expr;
-pub mod stmt;
-pub use expr::*;
-use std::any::Any;
-use std::collections::VecDeque;
-use std::fmt::Debug;
-pub use stmt::*;
-use tokenize::Token;
+pub use std::collections::HashSet;
+pub use std::rc::Rc;
 
-#[derive(Debug, PartialEq)]
-pub enum ParseError {
-    UnexpectedToken(Token),
-    ExpectedToken {
-        line: usize,
-        pos: usize,
-    },
-    UnclosedParen {
-        line: usize,
-        pos: usize,
-    },
-    UnendedStmt {
-        line: usize,
-        pos: usize,
-    },
-    SymbolAlreadyExists {
-        name: String,
-        line: usize,
-        pos: usize,
-    },
+/// Expression
+///
+/// \<expr\> ::= \<no-block-expr\> ";" | \<with-block-expr\> ";"?
+///
+/// \<no-block-expr\> must be followed by a semicolon, while
+/// \<with-block-expr\> may or may not be followed by a semicolon.
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr {
+    WithBlock(WithBlockExpr),
+    NoBlock(NoBlockExpr),
+}
+
+/// Expression without block.
+///
+/// \<no-block-expr\> ::= \<binary\>
+#[derive(Debug, Clone, PartialEq)]
+pub enum NoBlockExpr {
+    /// \<lit\> ::= STRING
+    ///            | NUMBER
+    ///            | FLOAT
+    ///            | "(" \<expr\> ")"
+    ///
+    /// If matching the rule "(" \<expr\> ")", forward to [`NoBlockExpr`]'s rule.
+    /// Otherwise, return this variant if parsing succeeds.
+    Literal(LiteralExpr),
+    /// \<proc-call\> ::= \<lit\> (\<param-lst\> | "." IDENTIFIER)
+    ///
+    /// \<param-lst\> ::= "(" \<params\>? ")"
+    /// \<params\> ::= \<expr\> ("," \<expr\>)*
+    ///
+    /// If matching the rule "." IDENTIFIER, forward to [`NoBlockExpr::Access`]
+    /// rule. Otherwise, return this variant if parsing succeeds.
+    ProcCall(ProcCallExpr),
+    /// \<access\> ::= \<lit\> "." IDENTIFIER
+    Access(AccessExpr),
+    /// \<unary\> ::= ("+" | "-" | "!")? \<lit\>
+    ///
+    /// If there is ("+" | "-" | "!"), return this variant if parsing succeeds,
+    /// otherwise, forward to [`NoBlockExpr::Literal`]'s rule. Otherwise,
+    /// return this variant if parsing succeeds.
+    Unary(UnaryExpr),
+    /// \<binary\> ::= \<assignment\>
+    /// \<assignment\> ::= \<or-or\> ("=" \<expr\>)?
+    /// \<or-or\> ::= \<and-and\> ("||" \<and-and\>)?
+    /// \<and-and\> ::= \<bit-or\> ("&&" \<bit-or\>)?
+    /// \<bit-or\> ::= \<bit-xor\> ("|" \<bit-xor\>)?
+    /// \<bit-xor\> ::= \<bit-and\> ("^" \<bit-and\>)?
+    /// \<bit-and\> ::= \<term\> ("&" \<term\>)?
+    /// \<term\> ::= \<factor\> (("+" | "-") \<factor\>)?
+    /// \<factor\> ::= \<unary\> (("*" | "/") \<unary\>)?
+    ///
+    /// If only a single \<unary\> is matched, forward to [`NoBlockExpr::Unary`]'s
+    /// rule. Otherwise, return this variant if parsing succeeds.
+    Binary(BinaryExpr),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum LiteralExpr {
+    String(Box<str>),
+    Integer(u64),
+    Float(f64),
+    Identifier(Box<str>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ProcCallExpr {
+    pub id: Box<str>,
+    pub params: Vec<Expr>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct AccessExpr {
+    pub lhs: Box<Expr>,
+    pub rhs: Box<str>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct UnaryExpr {
+    pub op: UnaryOp,
+    pub val: Box<Expr>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum UnaryOp {
+    Plus,
+    Minus,
+    Bang,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct BinaryExpr {
+    pub op: BinaryOp,
+    pub lhs: Box<Expr>,
+    pub rhs: Box<Expr>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum BinaryOp {
+    /// +
+    Plus,
+    /// -
+    Minus,
+    /// *
+    Mul,
+    /// /
+    Div,
+    /// &
+    And,
+    /// ^
+    XOr,
+    /// |
+    Or,
+    /// ==
+    EqEq,
+    /// !=
+    NEq,
+    /// <
+    Lt,
+    /// >
+    Gt,
+    /// <=
+    Le,
+    /// >=
+    Ge,
+    /// =
+    Eq,
+}
+
+/// Expression with block(s).
+///
+/// \<with-block-expr\> ::= \<block\> | \<if\> | \<while\>
+#[derive(Debug, Clone, PartialEq)]
+pub enum WithBlockExpr {
+    /// \<block\> ::= "{" \<stmt\>* "}"
+    Block(BlockExpr),
+    /// \<if\> ::= "if" \<expr\> \<block\>
+    ///                ("elif" \<expr\> \<block\>)*
+    ///                ("else" \<block\>)?
+    If(IfExpr),
+    /// \<while\> ::= "while" \<expr\> \<block\>
+    While(WhileExpr),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct BlockExpr {
+    pub stmts: Vec<Stmt>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct IfExpr {
+    pub if_branch: IfBranch,
+    // maybe we should use the same type to represent both if and elif branches.
+    pub elif_branches: Vec<ElifBranch>,
+    // there isn't a condition for `ElseBranch`.
+    pub else_block: Option<Box<Expr>>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct IfBranch {
+    pub cond: Box<Expr>,
+    pub block: Box<Expr>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ElifBranch {
+    pub cond: Box<Expr>,
+    pub block: Box<Expr>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct WhileExpr {}
+
+/// Statement.
+#[derive(Debug, Clone, PartialEq)]
+pub enum Stmt {
+    Expr(ExprStmt),
+    Decl(DeclStmt),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum DeclStmt {
+    Let(LetStmt),
+    LetMut(MutableLetStmt),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ExprStmt {
+    NoBlock,
+    Block,
+}
+
+// Using Rc<str> for identifier name here instead of Box<str> because we will
+// also be putting the names inside a HashMap.
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct LetStmt {
+    pub id: Rc<str>,
+    pub val: LetDefn,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct MutableLetStmt {
+    pub id: Rc<str>,
+    pub val: Expr,
+}
+
+/// Valid right-hand side for [`LetStmt`].
+/// Includes [`Expr`] and type/procedure definitions.
+#[derive(Debug, Clone, PartialEq)]
+pub enum LetDefn {
+    Expr(Expr),
+    Proc(ProcDefn),
+    Struct(StructDefn),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ProcDefn {
+    pub name: Rc<str>,
+    pub params: Vec<Rc<str>>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct StructDefn {
+    pub name: Rc<str>,
+    pub members: HashSet<Rc<str>>,
 }
