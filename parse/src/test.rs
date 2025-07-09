12534df212b3f808844cf5634862bac7a4c9542e
Huy Nguyen <huy55465@gmail.com>
2025-07-09 16:32:56 -0400

    test(parse): all currently valid unary signs
    Signed-off-by: Huy Nguyen <huy55465@gmail.com>

--- parse/src/test.rs
+++ parse/src/test.rs
@@ -1,13 +1,58 @@
 #![allow(clippy::wildcard_imports)]
-use std::collections::VecDeque;
+use std::{
+    collections::{HashMap, VecDeque},
+    sync::LazyLock,
+};
 
 use tokenize::{Token, TokenKind, tokens::CharPosition};
 
-use crate::{Expr, LiteralExpr, NoBlockExpr, UnaryExpr, parser::ParseExpr};
+use crate::{
+    Expr, LiteralExpr, NoBlockExpr, ParseError, UnaryExpr, UnaryOp,
+    parser::ParseExpr,
+};
 
 /// Token list is inferred to be having one space between each token
 fn build_token_deque(tokens: &[(TokenKind, Option<&str>)]) -> VecDeque<Token> {
     let mut column = 0;
+    static TOKEN_LENS: LazyLock<HashMap<TokenKind, usize>> =
+        LazyLock::new(|| {
+            #[allow(clippy::wildcard_imports)]
+            use TokenKind::*;
+            // since we have a lot of single-character tokens.
+            let single_char_tokens = [
+                Plus, Dash, Star, Slash, Ampersand, Hat, Beam, Backslash,
+                LParen, RParen, LBrace, RBrace, Dot, Colon, Semicolon, Comma,
+                Equal, Less, Greater,
+            ];
+            let mut ret = HashMap::new();
+            ret.extend(single_char_tokens.map(|kind| (kind, 1)).into_iter());
+            ret.extend(
+                [
+                    (LessEqual, 2),
+                    (GreaterEqual, 2),
+                    (Ya, 2),
+                    (Na, 2),
+                    (And, 3),
+                    (Or, 2),
+                    (Not, 3),
+                    (Eq, 2),
+                    (Neq, 3),
+                    (If, 2),
+                    (Elif, 4),
+                    (Else, 4),
+                    (Let, 3),
+                    (While, 5),
+                    (Def, 3),
+                    (Mut, 3),
+                    (Proc, 4),
+                    (Return, 6),
+                    (BlockReturn, 12),
+                ]
+                .into_iter(),
+            );
+            ret
+        });
+
     tokens
         .iter()
         .copied()
@@ -16,7 +61,13 @@ fn build_token_deque(tokens: &[(TokenKind, Option<&str>)]) -> VecDeque<Token> {
             pos: CharPosition {
                 line: 1,
                 column: {
-                    repr.inspect(|s| column += s.len() + 1);
+                    match repr {
+                        Some(s) => column += s.len(),
+                        None => {
+                            column +=
+                                TOKEN_LENS.get(&kind).copied().unwrap_or(1usize)
+                        }
+                    }
                     column
                 },
             },
@@ -83,4 +134,55 @@ fn parse_unary() {
             )))
         );
     }
+    // with all valid unary signs
+    {
+        let mut deque = build_token_deque(&[
+            (TokenKind::Not, None),
+            (TokenKind::Integer, Some("123")),
+            (TokenKind::Dash, None),
+            (TokenKind::Integer, Some("123")),
+            (TokenKind::Plus, None),
+            (TokenKind::Integer, Some("123")),
+        ]);
+        let asts = {
+            let mut ret = Vec::new();
+            let mut prev_pos = CharPosition { line: 1, column: 1 };
+            while !deque.is_empty() {
+                ret.push(
+                    UnaryExpr::parse_tokens(&mut deque, prev_pos)
+                        .inspect(|(_, pos)| prev_pos = *pos)
+                        .map(|(ast, _)| ast)
+                        .unwrap(),
+                );
+            }
+            ret
+        };
+        assert_eq!(
+            asts,
+            [
+                UnaryExpr {
+                    op: UnaryOp::Negate,
+                    val: Box::new(Expr::NoBlock(NoBlockExpr::Literal(
+                        LiteralExpr::Integer(123)
+                    ))),
+                },
+                UnaryExpr {
+                    op: UnaryOp::Minus,
+                    val: Box::new(Expr::NoBlock(NoBlockExpr::Literal(
+                        LiteralExpr::Integer(123)
+                    ))),
+                },
+                UnaryExpr {
+                    op: UnaryOp::Plus,
+                    val: Box::new(Expr::NoBlock(NoBlockExpr::Literal(
+                        LiteralExpr::Integer(123)
+                    ))),
+                }
+            ]
+            .map(|val| Expr::NoBlock(NoBlockExpr::Unary(val)))
+        );
+    }
+    // invalid unary sign cannot be tested until we're done with everything
+    // else. Because, something like `if 1 == 1 { 1 } else { 2 }` is valid.
+    // And, `if` isn't a valid unary sign, but this is a valid expression.
 }
